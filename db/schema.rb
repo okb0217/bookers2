27e506a2d0f347b90dedae992ce39758426211cb
okb0217 <scandal217.@gmail.com>
2020-03-07 04:17:44 +0000

    [finish]app3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_100718) do
+ActiveRecord::Schema.define(version: 2020_03_06_100454) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
